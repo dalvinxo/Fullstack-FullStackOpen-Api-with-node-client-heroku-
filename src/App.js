d640bb1d04a0823ded85981ae63c43e95082369e
Dalvin Molina <dalvingamer@gmail.com>
2022-04-18 17:54:03 -0400

    7.7 complete

--- src/App.js
+++ src/App.js
@@ -1,60 +1,30 @@
 import { useState } from 'react'
-import { Route, Routes, useMatch } from 'react-router-dom'
 
-import Anecdote from './components/Anecdote'
-import AnecdoteList from './components/AnecdoteList'
-import Menu from './components/Menu'
-import About from './components/About'
-import Footer from './components/Footer'
-import Notification from './components/Notification'
-import FormAnecdote from './components/FormAnecdote'
+import Country from './components/Country'
 
-import { anecdotes as anecdotesExample } from './helpers/listHelpers'
-
+import { useField, useCountry } from './hooks'
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState(anecdotesExample)
 
-  const [notification, setNotification] = useState('')
+  const nameInput = useField('text')
 
-  const match = useMatch('/anecdotes/:id')
+  const [name, setName] = useState('')
+  
+  const country = useCountry(name)
 
-  const addNew = (anecdote) => {
-    anecdote.id = Math.round(Math.random() * 10000)
-    setAnecdotes(anecdotes.concat(anecdote))
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
   }
-
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
-
-  const anecdote = match ? anecdoteById(Number(match.params.id)) : null;
 
   return (
     <div>
-    
-      
-        <h1>Software anecdotes</h1>
-        <Menu />
-        <Notification notification={notification} setNotification={setNotification}/>
-        <Routes>
-          <Route path='/create' element={<FormAnecdote addNew={addNew} setNotification={setNotification}/>} />
-          <Route path='/about' element={<About />} />
-          <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} vote={vote} />} />
-          <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
-        </Routes>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
 
-        <Footer />
+      <Country country={country} />
     </div>
   )
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const useField = (type) => {
-    const [value, setValue] = useState('');
-
+    const [value, setValue] = useState('')
+  
     const onChange = (event) => {
-        setValue(event.target.value);
+      setValue(event.target.value)
     }
-
-    const reset = () => {
-        setValue('');
+  
+    return {
+      type,
+      value,
+      onChange
     }
-
-    return {type, value, onChange, reset};
-}
-
-
+  }
+  
+export const useCountry = (name) => {
+    const [country, setCountry] = useState(null)
+  
+    useEffect(() => {  
+        axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(res => setCountry(res.data[0]))
+        .catch(error => { 
+            console.error(error) 
+            setCountry(null)
+        })
+    }, [name])
+  
+    return country
+  }
+  
