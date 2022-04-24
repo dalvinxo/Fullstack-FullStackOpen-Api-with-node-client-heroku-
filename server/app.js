2c294c99f85db044d5c0dfeac6bb14eff1436b07
Dalvin Molina <dalvingamer@gmail.com>
2022-04-24 09:12:54 -0400

    feat: middleware add error, unknow, logger

--- server/app.js
+++ server/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 
 const blogRoute = require('./controllers/blog.controller');
+const middleware = require('./utils/middleware.util');
 
 const app = express();
 
 app.use(express.json());
+app.use(middleware.logger);
 
 app.use('/api/blogs', blogRoute);
+
+app.use(middleware.handleError);
+app.use(middleware.handleUnknownEndpoint);
 
 module.exports = app;
---
+++ server/utils/middleware.util.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan');
+
+morgan.token('body', (req) => (req.body ? JSON.stringify(req.body) : 'Body is empty'));
+
+const handleError = (error, response, request, next) => {
+
+	if (error.name === 'ValidationError') {
+		return response.status(400).json({
+			error: error.message
+		});
+	}
+
+	next(error);
+};
+
+const handleUnknownEndpoint = (request, response) => {
+	response.status(404).json({
+		error: 'Unknown endpoint'
+	});
+};
+
+
+module.exports = {
+	logger: morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+	handleError,
+	handleUnknownEndpoint
+};
